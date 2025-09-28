18ce9cbbead5f3dba7217c35754bc970f558f87d
PiekharievaK <pechareva@gmail.com>
2025-09-28 16:29:07 +0300

    add swagger plugin

---
+++ src/plugins/swagger.plugin.ts
@@ -0,0 +1,34 @@
+import fp from "fastify-plugin";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
+
+export default fp(async (fastify, opts) => {
+	const pluginName = "swagger-plugin";
+	await fastify.register(swagger, {
+		openapi: {
+			openapi: "3.0.0",
+			info: {
+				title: "news-server",
+				description: "Auto-generated Swagger docs",
+				version: "1.0.0",
+			},
+			servers: [
+				{
+					url: fastify.config.SERVER_URL,
+				},
+			],
+		},
+	});
+
+	await fastify.register(swaggerUI, {
+		routePrefix: "/docs",
+		uiConfig: {
+			docExpansion: "list",
+			deepLinking: true,
+		},
+		staticCSP: true,
+		transformSpecificationClone: true,
+	});
+
+	fastify.pluginLoaded(pluginName);
+});
